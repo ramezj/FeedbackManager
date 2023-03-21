0266780c0b7900f4880d0560cde263165729abc4
Lunix <ramezjoseph8@gmail.com>
2023-03-21 08:22:57 +0200

    Prisma Updates

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,13 +6,16 @@ const Dashboard = () => {
     const router = useRouter()
     const { data: session, status } = useSession()
       if (status === "unauthenticated") {
+        // don't return router.push
         router.push('/unauthorized')
+        return null;
       }
     
       return (
         <>
-          <h1>Protected Page</h1>
-          <p>You can view this page because you are signed in.</p>
+          <center>
+            <h2>{JSON.stringify(session)}</h2>
+          </center>
         </>
       )
 }
