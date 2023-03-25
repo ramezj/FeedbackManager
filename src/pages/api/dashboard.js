96ec71b09df3527559f8d50c4ae4022afd246cc0
Lunix <ramezjoseph8@gmail.com>
2023-03-25 12:35:54 +0200

    Dashboard Integration

---
+++ src/pages/api/dashboard.js
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
+export default async function handler(req, res) {
+    const prisma = new PrismaClient();
+    const { body, method } = req;
+    const { username, email, password } = req.body;
+    if (method !== "GET") {
+        return res.status(405).json({
+            message: "Method not allowed",
+          });
+    }
+    const token = req.body.token;
+    if (!token) {
+        return res.status(400).json({
+            message:"Missing token."
+        });
+    } else {
+        try {
+            const verifyToken = await jwt.verify(token, process.env.JWT_SECRET);
+            if(!verifyToken) {
+                return res.status(400).json({
+                    message:"Couldn't Verify Token"
+                })
+            } else {
+                const user = await prisma.user.findUnique({
+                    where: {
+                         id:verifyToken.id
+                    }
+                })
+                return res.status(200).json({
+                    user
+                })
+            }   
+        } catch (error) {
+            return res.status(400).json({
+                message:error.message
+            })
+        }
+    }
+  }
