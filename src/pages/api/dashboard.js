667f7cde108927a609a4b53f6fa646b0c65de085
Lunix <ramezjoseph8@gmail.com>
2023-03-28 22:10:26 +0200

    more vercel debugging

--- src/pages/api/dashboard.js
+++ src/pages/api/dashboard.js
@@ -46,8 +46,9 @@ export default async function handler(req, res) {
                 })
             }   
         } catch (error) {
+            console.log(error);
             return res.status(400).json({
-                message:error
+                message:error.message
             })
         }
     }
