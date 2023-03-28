08334bb160df024d38d69c4e2a8b3c718a4024a2
Lunix <ramezjoseph8@gmail.com>
2023-03-28 21:05:55 +0200

    Vercel /register

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -16,7 +16,7 @@ const RegisterForm = () => {
             email:email,
             password: password
         }
-        const response = await fetch('/api/auth/Register', {
+        const response = await fetch('/api/auth/register', {
             method:'POST',
             headers: {
                 "Content-Type": "application/json",
