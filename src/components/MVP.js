4f53b2570e2aabad869a1252c437d4067b75c52a
Lunix <ramezjoseph8@gmail.com>
2023-04-01 17:19:14 +0200

    MVP!

--- src/components/MVP.js
+++ src/components/MVP.js
@@ -3,19 +3,21 @@ import useSWR from 'swr';
 import Cookies from 'universal-cookie';
 import { useRouter } from 'next/router'
 import { Input } from "@nextui-org/react";
+import { motion } from 'framer-motion';
 
 const MVP = () => {
     return (
     <div> 
-<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div class="shadow-xl max-w-sm p-6 bg-zinc-900 rounded-lg shadow dark:bg-gray-950">
     <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Whats on your mind? 🔨 </h5>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">Whats on your mind? 🔨 </h5>
+        <br></br>
     </a>
-<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+<textarea id="message" rows="4" class="shadow-xl block p-2.5 w-full text-sm text-white bg-zinc-800 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 <br></br>
-    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Send Feedback 🔖
-    </a>
+<motion.button whileHover={{scale: 1.1}} class="bg-blue-700 duration-500 shadow-xl px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors transform rounded-lg">
+Send Feedback 🔖
+</motion.button>
 </div>
 
     </div>  
