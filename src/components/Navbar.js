b9ac53a631ee88079e3838a684b03d9b3348034c
Lunix <ramezjoseph8@gmail.com>
2023-04-03 03:17:16 +0200

    small update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,12 +13,15 @@ const Navbar = (props) => {
   const redirectSettings = () => {
     router.push('/settings')
   }
+  const redirectHome = () => {
+    router.push('/')
+  }
     return (
         <nav class="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 left-0">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center">
+  <a onClick={redirectHome} class="cursor-pointer flex items-center">
       <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo"/>
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Feedbacker</span>
   </a>
   <div class="flex md:order-2 space-x-3">
   
