5d6bfb828e4f3467b343add85672b17a2b279f94
Lunix <ramezjoseph8@gmail.com>
2023-04-11 13:15:05 +0200

    debug

--- src/pages/api/callback.js
+++ src/pages/api/callback.js
@@ -14,34 +14,22 @@ export default async function handler(req, res) {
     if(req.body.alert_name == "subscription_created") {
       // Update User Logic Here. 
       try {
-        const userExist = await prisma.user.findUnique({
-          where: {
+        const updateUser = await prisma.user.update({
+          where:{
             id:req.body.passthrough
+          },
+          data: {
+            isSubscribed:true
           }
         });
-        if(userExist) {
-          const updateUser = await prisma.user.update({
-            where:{
-              id:req.body.passthrough
-            },
-            data: {
-              isSubscribed:true
-            }
-          });
-          if(updateUser) {
-            res.status(200).json({ok:true, message:"Subscribed Successfully! :D"});
-          } else {
-            res.status(401).json({ok:false, message:"Couldn't subscribe user.."})
-          }
-        } else {
-          return res.status(404).json({message:"Couldn't find User.."})
+        if(updateUser) {
+          return res.status(200).json({ok:true, message:"Subscribed Succesfully"})
         }
       } catch (error) {
-        res.status(400).json({error});
+        return res.status(404).json({ok:false, error});
       }
-      return res.status(200).json("Subscription Created Successfully!")
     } else {
-      return res.status(200).json({
+      return res.status(400).json({
         message:"Couldn't Verify Subscription"
       })
     }
