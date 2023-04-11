311ab9dc4ca553cdf58d5b10cc0ed604f7ff2664
Lunix <ramezjoseph8@gmail.com>
2023-04-11 13:09:34 +0200

    test callback

--- src/pages/api/callback.js
+++ src/pages/api/callback.js
@@ -12,7 +12,33 @@ export default async function handler(req, res) {
           });
     }
     if(req.body.alert_name == "subscription_created") {
-      // Update User Logic Here.
+      // Update User Logic Here. 
+      try {
+        const userExist = await prisma.user.findUnique({
+          where: {
+            id:req.body.passthrough
+          }
+        });
+        if(userExist) {
+          const updateUser = await prisma.user.update({
+            where:{
+              id:req.body.passthrough
+            },
+            data: {
+              isSubscribed:true
+            }
+          });
+          if(updateUser) {
+            res.status(200).json({ok:true, message:"Subscribed Successfully! :D"});
+          } else {
+            res.status(401).json({ok:false, message:"Couldn't subscribe user.."})
+          }
+        } else {
+          return res.status(404).json({message:"Couldn't find User.."})
+        }
+      } catch (error) {
+        res.status(400).json({error});
+      }
       return res.status(200).json("Subscription Created Successfully!")
     } else {
       return res.status(200).json({
