38c17ecc16cf2746e9136985318e1e87ee197090
Lunix <ramezjoseph8@gmail.com>
2023-04-15 12:38:40 +0200

    log request headers

--- src/pages/api/receiveFeedback.js
+++ src/pages/api/receiveFeedback.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import { NextRequest } from 'next/server'
 
 export default async function handler(req, res) {
-    console.log(req);
+    console.log(req.headers);
     const prisma = new PrismaClient();
     const { body, method } = req;
     if (method !== "POST") {
