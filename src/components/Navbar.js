7a5da5b3e29934fe901e51b294475a03f4e99ad7
Lunix <ramezjoseph8@gmail.com>
2023-04-21 13:27:33 +0200

    nav update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -53,7 +53,7 @@ const NavbarComponent = (props) => {
         <li><button class="ml-1 btn btn-ghost normal-case text-md mt-2 text-black" onClick={redirectPricing}><a>Pricing</a></button></li>
       </ul>
     </div>
-    <button onClick={redirectHome} className="ml-1 btn btn-ghost normal-case text-xl text-black">HeyFeedback!</button>
+    <button onClick={redirectHome} className="ml-1 btn btn-ghost normal-case text-xl text-black">Hey<b>Feedback</b></button>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -102,7 +102,7 @@ const NavbarComponent = (props) => {
         <li><button class="ml-1 btn btn-ghost normal-case text-md mt-2 text-black" onClick={redirectPricing}><a>Pricing</a></button></li>
       </ul>
     </div>
-    <button onClick={redirectHome} className="ml-1 btn btn-ghost normal-case text-xl text-black">HeyFeedback!</button>
+    <button onClick={redirectHome} className="ml-1 btn btn-ghost normal-case text-xl text-black">Hey<b>Feedback</b></button>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
